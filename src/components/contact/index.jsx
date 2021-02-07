da04ae5947278545102e782c60aad364a99e7ee5
cesar <cablandon89@gmail.com>
2021-02-07 16:17:33 -0500

    Puliendo readme

--- src/components/contact/index.jsx
+++ src/components/contact/index.jsx
@@ -9,6 +9,7 @@ const Contact = () => {
         <h3>Visitanos</h3>
         <p>Parque metropolitano del café - local 15</p>
         <div class="mapouter"><div class="gmap_canvas"><iframe class="gmap_iframe" width="100%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=600&amp;height=400&amp;hl=en&amp;q=Parque metropolitano del cafe&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe><a href="https://piratebayproxy.net/">pirate bay proxy</a></div></div>
+        
      </div>
      
     </section>
